d0bb68fb23be5d34a750519891f5ae4af6051c86
Oliver Kraus <krauso@tcd.ie>
2021-10-24 17:13:15 +0100

    changed custom analyser again

--- src/main/java/CustomAnalyzer.java
+++ src/main/java/CustomAnalyzer.java
@@ -4,6 +4,7 @@ import org.apache.lucene.analysis.en.KStemFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.miscellaneous.HyphenatedWordsFilter;
+import org.apache.lucene.analysis.miscellaneous.PatternKeywordMarkerFilter;
 import org.apache.lucene.analysis.miscellaneous.TrimFilter;
 import org.apache.lucene.analysis.standard.ClassicFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
@@ -12,29 +13,29 @@ import org.apache.lucene.analysis.tr.ApostropheFilter;
 import java.util.Arrays;
 
 public class CustomAnalyzer extends Analyzer {
-    private String[] mStopWordList = {
+
+    private final String[] mStopWordList = {
             "a", "an", "and", "are","aren't", "as", "at", "be", "but", "by","can","can't", "does","how",
             "for", "if", "in", "into", "is", "it","have","haven't","why","has",
-            "no", "not", "of", "on", "or", "such",
-            "that", "the", "their", "then", "there", "these",
-            "they", "this", "to", "what", "was", "will", "with"
+            "no", "not", "of", "on", "or", "such","under","over","out",
+            "that", "the", "their", "then","them", "there", "these",
+            "they", "this", "to","too", "what", "was", "will", "with","where"
     };
 
     private CharArraySet mStopWordCharArrayList = new CharArraySet(Arrays.asList(mStopWordList),true);
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-
         final Tokenizer tokenizer = new StandardTokenizer();
         TokenStream tokenStream = new ClassicFilter(tokenizer);
         tokenStream = new LowerCaseFilter(tokenStream);
+        tokenStream = new TrimFilter(tokenStream);
         tokenStream = new EnglishPossessiveFilter(tokenStream);
         tokenStream = new HyphenatedWordsFilter(tokenStream);
-        tokenStream = new TrimFilter(tokenStream);
         tokenStream = new ASCIIFoldingFilter(tokenStream);
-        tokenStream = new StopFilter(tokenStream, mStopWordCharArrayList);
-        //tokenStream = new PorterStemFilter(tokenStream);
+        tokenStream = new PorterStemFilter(tokenStream);
         tokenStream = new KStemFilter(tokenStream);
+        tokenStream = new StopFilter(tokenStream, mStopWordCharArrayList);
         return new TokenStreamComponents(tokenizer, tokenStream);
     }
 
