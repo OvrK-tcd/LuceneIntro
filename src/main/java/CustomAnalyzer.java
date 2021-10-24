8ab4f326f81964fc9c0ede19a5b062b20eea306a
Oliver Kraus <krauso@tcd.ie>
2021-10-24 16:54:18 +0100

    changed custom analyser

--- src/main/java/CustomAnalyzer.java
+++ src/main/java/CustomAnalyzer.java
@@ -4,43 +4,46 @@ import org.apache.lucene.analysis.en.KStemFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.miscellaneous.HyphenatedWordsFilter;
-import org.apache.lucene.analysis.miscellaneous.LengthFilter;
 import org.apache.lucene.analysis.miscellaneous.TrimFilter;
 import org.apache.lucene.analysis.standard.ClassicFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.tr.ApostropheFilter;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-
 public class CustomAnalyzer extends Analyzer {
+    private String[] mStopWordList = {
+            "a", "an", "and", "are","aren't", "as", "at", "be", "but", "by","can","can't", "does","how",
+            "for", "if", "in", "into", "is", "it","have","haven't","why","has",
+            "no", "not", "of", "on", "or", "such",
+            "that", "the", "their", "then", "there", "these",
+            "they", "this", "to", "what", "was", "will", "with"
+    };
+
+    private CharArraySet mStopWordCharArrayList;
+    CustomAnalyzer() {
+        super();
+        for (String s : mStopWordList) {
+            mStopWordCharArrayList.add(s);
+        }
+    }
+
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
+
         final Tokenizer tokenizer = new StandardTokenizer();
         TokenStream tokenStream = new ClassicFilter(tokenizer);
-        tokenStream = new EnglishPossessiveFilter(tokenStream);
-        tokenStream = new ASCIIFoldingFilter(tokenStream);
         tokenStream = new LowerCaseFilter(tokenStream);
-        tokenStream = new TrimFilter(tokenStream);
-        tokenStream = new StopFilter(tokenStream, getStopWprds());
+        tokenStream = new EnglishPossessiveFilter(tokenStream);
         tokenStream = new HyphenatedWordsFilter(tokenStream);
-        tokenStream = new PorterStemFilter(tokenStream);
+        tokenStream = new TrimFilter(tokenStream);
+        tokenStream = new ASCIIFoldingFilter(tokenStream);
+        System.out.println("stop Word list size: " + mStopWordCharArrayList.size());
+        tokenStream = new StopFilter(tokenStream, mStopWordCharArrayList);
+        //tokenStream = new PorterStemFilter(tokenStream);
         tokenStream = new KStemFilter(tokenStream);
         return new TokenStreamComponents(tokenizer, tokenStream);
     }
 
-    private CharArraySet getStopWprds(){
-        CharArraySet stopwords = null;
-        try {
-            byte[] encoded = Files.readAllBytes(Paths.get("../../../src/stopwords.txt"));
-            String[] words = new String(encoded, StandardCharsets.UTF_8).split("\n");
-            stopwords =  new CharArraySet(Arrays.asList(words), true);
-        } catch (IOException ioe) {
-            System.out.println("Error Reading stopwords file");
-        }
-        return stopwords;
-    }
+
+
+
 }
